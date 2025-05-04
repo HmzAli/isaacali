e75b7896a41aa4d7f36a624d6e6639bc67ffe5f4
Hamza Ali <plomenet@gmail.com>
2025-05-04 14:23:11 +0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import { Link } from 'react-router'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Isaac Ali portfolio</h1>
