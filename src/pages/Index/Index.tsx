0a50295351f6ff943f2a39375e9b7cf9ff56eb41
Hamza Ali <plomenet@gmail.com>
2025-05-05 20:15:12 +0800

    about component

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -2,6 +2,7 @@ import Layout from '../../components/Layout'
 import MainBanner from '../../components/MainBanner'
 import Services from '../../components/Services'
 import Selections from '../../components/Selections'
+import About from '../../components/About'
 import Testimonials from '../../components/Testimonials'
 import Contact from '../../components/Contact'
 import './Index.scss'
@@ -12,6 +13,7 @@ function Index() {
       <MainBanner />
       <Services />
       <Selections />
+      <About />
       <Testimonials />
       <Contact />
     </Layout>
