50542b907a593c53c83a0d78d41be0ba12f23879
Hamza Ali <plomenet@gmail.com>
2025-07-27 23:12:55 +0300

    updates

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -6,7 +6,7 @@ const Services = () => {
       <div className="container">
         <h2 className="main-section__title" data-aos="fade-in" data-aos-duration="500" data-aos-delay="200">What I offer</h2>
 
-        <div className="service-wrapper"  data-aos="fade-in" data-aos-duration="800" data-aos-delay="300">
+        <div className="service-wrapper" data-aos="fade-in" data-aos-duration="800" data-aos-delay="300">
           <div className="service-item" data-content-id="service-item-1">
             <div className="service-item__content">
                 <p>
@@ -17,7 +17,7 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="mock-wrapper">
+      <div className="mock-wrapper" data-aos="fade-in" data-aos-duration="1000" data-aos-delay="200">
         <img className="phone-mock" src="/phone-mock.webp" alt="Phone" />
         <img className="laptop-mock" src="/laptop-mock.webp" alt="Phone" />
       </div>
